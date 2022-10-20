8ea12a64c383d35134416561ef7532535ace2171
Mel O'Hagan <mel@budibase.com>
2022-10-20 11:25:51 +0100

    init

---
+++ src/datasource.ts
@@ -0,0 +1,59 @@
+import { IntegrationBase, SqlQuery } from "@budibase/types"
+import Stripe from "stripe"
+
+class CustomIntegration implements IntegrationBase {
+  private readonly stripe: Stripe
+
+  constructor(config: { apiKey: string; }) {
+    this.stripe = new Stripe(config.apiKey, {
+      apiVersion: '2022-08-01'
+    })
+  }
+
+  async create(query: { 
+    address: object,
+    description: string,
+    email: string,
+    metadata: object,
+    name: string,
+    payment_method: string,
+    phone: string,
+    shipping: object
+   }) {
+    return await this.stripe.customers.create(query as Stripe.CustomerCreateParams)
+  }
+
+  async read(query: { id: string, email: string, ending_before: string, limit: number, starting_after: string }) {
+    if (query.id) {
+      return await this.stripe.customers.retrieve(query.id)
+    }
+    return await this.stripe.customers.list(query)
+  }
+
+  async update(query: {
+    id: string, 
+    address: object,
+    description: string,
+    email: string,
+    metadata: object,
+    name: string,
+    payment_method: string,
+    phone: string,
+    shipping: object
+   }) {
+    const { id, ...params } = query
+    return await this.stripe.customers.update(id, params as Stripe.CustomerUpdateParams)
+  }
+
+  async delete(query: { id: string }) {
+    return await this.stripe.customers.del(query.id)
+  }
+
+  async search(query: SqlQuery) {
+    return await this.stripe.customers.search({
+      query: query.sql
+    })
+  }
+}
+
+export default CustomIntegration
